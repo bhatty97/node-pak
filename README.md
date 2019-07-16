0e494417934695838735a236b23995e0c3ff7352
bhatty97 <yash.v.bhatt97@gmail.com>
2019-07-16 16:07:58 -0400

    Changing questions while adding a full quiz option

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
-![alt text](https://raw.githubusercontent.com/hanywang2/hanny/master/img/hanny.png "Logo Title Text 1")
-[![Build](https://travis-ci.org/hanywang2/hanny.svg?branch=master)](https://travis-ci.org/hanywang2/hanny)
-  
 ## Quick Start
 Package installation can be done with [`npm install` command](https://docs.npmjs.com/getting-started/installing-npm-packages-locally):
 ```
-$ npm install -g hanny
+$ npm i -g bhatty97
 ```
 
 ## Usage
 Start the quiz using the following CLI command:
 ```
-$ hanny
+$ yash
 ```
 
 ## Local Use
 ```js
-const han = require('hanny');
+const yash = require('yash');
 
-han.randomQuestion();
-// Returns a random question {query: 'What is Han's favorite color?', answer: 'Red'}
+yash.randomQuestion();
+// Returns a random question {query: 'What is Yash's's favorite color?', answer: 'Red'}
 ```
 
-## Licsense
-[MIT](LICENSE) © Han Wang
+## License
+[MIT](LICENSE) © Yash Bhatt
 
--- bin/index.js
+++ bin/index.js
@@ -6,15 +6,16 @@ const qoa = require('qoa');
 const {log} = console;
 
 const ques = require('../lib/questions')
-    
-log(chalk.red("  _   _                                     _   ___  "));
-log(chalk.red(" | | | | __ _ _ __  _ __  _   _    __   __ / | / _ \\ "));
-log(chalk.red(" | |_| |/ _` | '_ \\| '_ \\| | | |   \\ \\ / / | || | | |"));
-log(chalk.red(" |  _  | (_| | | | | | | | |_| |    \\ V /  | || |_| |"));
-log(chalk.red(" |_| |_|\\__,_|_| |_|_| |_|\\__, |     \\_/   |_(_)___/ "));
-log(chalk.red("                          |___/                      "));
+log(chalk.red("          __     __      __                                     "));    
+log(chalk.red("          \\ \\   / /     /  \\       ________     | |        | |  "));
+log(chalk.red("           \\ \\ / /     / /\\ \\      | ______|    | |        | |  "));
+log(chalk.red("            \\   /     / /__\\ \\     | |_____     | |________| |  "));
+log(chalk.red("             | |     / /____\\ \\    |_____  |    | |________| |  "));
+log(chalk.red("             | |    / /      \\ \\    _____| |    | |        | |  "));
+log(chalk.red("             | |   / /        \\ \\  |_______|    | |        | |  "));
+log(chalk.red("                                                                "));
 log();
-log(chalk.bold("Welcome to Hanny, the best npm package about Han Wang."));
+log(chalk.bold("Welcome to the best npm package about Yash Bhatt!...I was too lazy to fix the letters"));
 
 (async function() {
     let conf = qoa.prompt([ques.begin]);
@@ -25,6 +26,63 @@ log(chalk.bold("Welcome to Hanny, the best npm package about Han Wang."));
     }
 
 })();
+async function roundfull() {
+    qoa.clearScreen();
+    log('\n')
+    let used = [];
+    let score = 0;
+
+    for (let i = 1; i <= 50; i++) {
+        let rand = Math.floor(Math.random() * ques.qs.length);
+
+        if (!used.includes(rand)) {
+            log(chalk.whiteBright.bgRed(`  Question ${i}/50:  `));
+            let question = qoa.prompt([ques.qs[rand]]);
+            let answer = await question;
+
+            if (ques.qs[rand].type === 'input') {
+                if (ques.qs[rand].answer === answer.a) {
+                    log(chalk.green('Correct'));
+                    score++;
+                } else {
+                    log(chalk.red('Wrong'));
+                    let longate = {t:'true', f:'false'};
+                    if (longate[ques.qs[rand].answer]) {
+                        log(chalk.gray(`The correct answer is ${longate[ques.qs[rand].answer]}`));
+                    } else {
+                        log(chalk.gray(`The correct answer is ${ques.qs[rand].answer}`));
+                    }
+                }
+            } else if (ques.qs[rand].type === 'quiz') {
+                if (answer.a.isCorrect) {
+                    log(chalk.green('Correct'));
+                    score++;
+                } else {
+                    log(chalk.red('Wrong'));
+                    log(chalk.gray(`The correct answer is ${ques.qs[rand].answer}`));
+                }
+            }
+            log('\n')
+
+            used.push(rand);
+
+        } else {
+            i--;
+        }
+
+    }
+    log(chalk.whiteBright.bgRed.bold(`  You got ${score} correct  `));
+    let fullquiz = qoa.prompt([ques.full]);
+    let res2 = await fullquiz
+    if (res2.type) {
+        roundfull();    
+    }
+    let another = qoa.prompt([ques.again]);        
+    let res = await another;
+    if (res.type) {
+        round();
+    }
+}
 
 async function round() {
     qoa.clearScreen();
@@ -32,11 +90,11 @@ async function round() {
     let used = [];
     let score = 0;
 
-    for (let i = 1; i <= 7; i++) {
+    for (let i = 1; i <= 10; i++) {
         let rand = Math.floor(Math.random() * ques.qs.length);
 
         if (!used.includes(rand)) {
-            log(chalk.whiteBright.bgRed(`  Question ${i}/7:  `));
+            log(chalk.whiteBright.bgRed(`  Question ${i}/10:  `));
             let question = qoa.prompt([ques.qs[rand]]);
             let answer = await question;
 
@@ -74,8 +132,13 @@ async function round() {
     log(chalk.whiteBright.bgRed.bold(`  You got ${score} correct  `));
     let another = qoa.prompt([ques.again]);
     let res = await another;
-
     if (res.type) {
         round();
+    }   
+    let fullquiz = qoa.prompt([ques.full]);
+    let res2 = await fullquiz
+    if (res2.type) {
+        roundfull();    
     }
+   
 }
